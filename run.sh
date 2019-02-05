9898476324362dfcac72b7e04ec672dca7fc4691
Dhiraj <dramnani@usc.edu>
2019-02-05 01:12:56 -0800

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-javac ./src/Main.java
-javac ./src/Prescriber.java
-java -cp ./src Main ./input/itcont.txt ./output/top_cost_drug.txt
+cd src
+javac -d .  Main.java
+javac -d . Prescriber.java
+java Main ../input/itcont.txt ../output/top_cost_drug.txt
